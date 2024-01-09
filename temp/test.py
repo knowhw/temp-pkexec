67a467c11e4dc70a22106ea0a15d5e9ba95d4426
knowhw <142631271+knowhw@users.noreply.github.com>
2024-01-09 22:15:46 +0000

    Update test.py

--- temp/test.py
+++ temp/test.py
@@ -2,13 +2,13 @@
 content = '''[PlankDockItemPreferences]
 Launcher=file:///usr/share/applications/geany.desktop
 '''
-
 # import temp
 # path='/home/elementary/.config/plank/dock1/launchers/geany.dockitem'
 
 # tempfile, filename = temp.filetouch(content)
-# tempfile.mv2(path)
 # tempfile.pkexec.mv2(path)
+
+# tempfile.mv2(path)
 # tempfile.delete(filename)
 
 
