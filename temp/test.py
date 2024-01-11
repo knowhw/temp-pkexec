3c8f64b5e84adeb3442661b2f3d8ddb944b406ef
knowhw <142631271+knowhw@users.noreply.github.com>
2024-01-11 16:34:00 +0000

    Update test.py

--- temp/test.py
+++ temp/test.py
@@ -7,16 +7,16 @@ Launcher=file:///usr/share/applications/geany.desktop
 
 # path='/home/elementary/.config/plank/dock1/launchers/geany.dockitem'
 
-# tempfile, filename = temp.filetouch(content)
+# tempfile, name = temp.filetouch(content)
 # tempfile.pkexec.mv2(path)
 
 # tempfile.mv2(path)
-# tempfile.delete(filename)
+# tempfile.delete(name)
 
 
 # filetouch => parametres (content)
 # pkexec.mv2 => parametres (path, deleted='true', user='root')
 # mv2 => parametres (path, deleted='true', user='home')
-# delete => parametres (filename)
-# _save_directory => default: /tmp/temporary.save
+# delete => parametres (name)
+# _save_directory => default: /tmp/cache.save
 
